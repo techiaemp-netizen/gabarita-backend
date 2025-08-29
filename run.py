8a30ecc7dad951a23f1165abdba911b725ce467d
techiaemp-netizen <techia.emp@gmail.com>
2025-08-29 09:24:47 -0300

    Add: Arquivos de configuração e dependências

---
+++ run.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Ponto de entrada principal para o backend do Gabarita AI
+"""
+
+import os
+import sys
+
+# Adicionar o diretório src ao path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+
+from main import app
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
